9db55c3e84953b0ca7567bd83ba2db1f8f64d488
Mansi Shah <129341395+mshah001@users.noreply.github.com>
2023-08-01 08:53:51 -0700

    CNDET-171-173  (#16)
    * CNDET-162 test auth publish service
    
    test auth publish service
    
    * updates
    
    * CNDET-171
    
    * Update .gitignore
    
    * DS_store cleanup
    
    * Update publish.feature
    
    * Updates
    
    Updates to validate generated csv
    
    * updates
    
    * Update README.md
    
    * update expected_csv
    
    * updated scenarios with failure
    
    * Updated expected_csv.csv

--- services/test/README.md
+++ services/test/README.md
@@ -4,9 +4,18 @@
 Using $PROJECT_ROOT reference as that in publish folder as starting point,
 build the docker image and run the behave script on that docker image.
 
-
+## Generate test data CSV from DDL
 ```shell
 $ cd $PROJECT_ROOT
 $ docker build -t mansi-python-app -f services/test/Dockerfile .
 $ docker run -it --rm -v $PROJECT_ROOT/services/test/episync_gen/docker_vol_shared:/docker_vol_shared --name my-running-app mansi-python-app
 ```
+Run behave test from services/test/episync 
+## XML to CSV convertor validation
+```shell
+$ behave features/publish.feature -f plain --no-capture
+```
+## html report for BDD test run
+````shell
+$ behave features/publish.feature -f html -o report.html
+````
--- services/test/episync_gen/features/steps/generate.py
+++ services/test/episync_gen/features/steps/generate.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import csv
 
 @given('I have url endpoint "{url}"')
 def step_impl(context, url):
@@ -61,3 +62,42 @@ def step_impl(context, method, file_path):
     response = requests.request("POST", context.url, headers=headers, data=payload, files=files)
     context.response = response
     print(response.text)
+
+@then(u'I read CSV')
+def step_impl(context):
+    location = context.response.headers['Location']
+    file_name = str(location).split("/")[-1]
+    # breakpoint()
+    with open("/Users/MansiShah/episync/"+file_name) as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=',')
+        for row in csvReader:
+            print(row)
+
+    context.response_file = "/Users/MansiShah/episync/"+file_name
+
+@then(u'compare generated csv with expected csv')
+def step_impl(context):
+
+    with open(context.response_file, 'r') as csv1, open('/Users/MansiShah/Work/NEDSS-EpiSync/services/test/episync_gen/expected_csv.csv', 'r') as csv2:
+        actual = csv1.readlines()
+        expected = csv2.readlines()
+
+    # Create CSV file with differences
+    with open('data_diff.csv', 'w') as outFile:
+        for row in expected:
+            if row not in actual:
+                outFile.write(row)
+                assert False, "CSV is not as expected"
+                # print("CSV is not as expected")
+
+            else:
+                print("CSV is as expected")
+
+
+
+
+
+
+
+
+
