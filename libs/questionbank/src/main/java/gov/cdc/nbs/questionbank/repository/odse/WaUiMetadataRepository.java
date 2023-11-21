48be31d42a1fd1a3087d5e0769534b6143c9873b
Sergei Veselev <sveselev@enquizit.com>
2023-11-21 15:55:10 -0500

    CNEDT-312 Update Genv2 template for changes using JSON data

--- libs/questionbank/src/main/java/gov/cdc/nbs/questionbank/repository/odse/WaUiMetadataRepository.java
+++ libs/questionbank/src/main/java/gov/cdc/nbs/questionbank/repository/odse/WaUiMetadataRepository.java
@@ -4,6 +4,9 @@ import gov.cdc.nbs.questionbank.entity.odse.WaUiMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface WaUiMetadataRepository extends JpaRepository<WaUiMetadata, Long>, JpaSpecificationExecutor<WaUiMetadata> {
 
+    List<WaUiMetadata> findAllByWaTemplateUidOrderByOrderNbr(Long uid);
 }
