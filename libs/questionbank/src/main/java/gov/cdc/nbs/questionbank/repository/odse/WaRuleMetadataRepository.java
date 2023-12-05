89a5d0e77b044af59983c18b0dc47635a6a47062
Sergei Veselev <sveselev@enquizit.com>
2023-12-05 16:26:38 -0500

    CNEDT-303 Handling question updates (#41)

--- libs/questionbank/src/main/java/gov/cdc/nbs/questionbank/repository/odse/WaRuleMetadataRepository.java
+++ libs/questionbank/src/main/java/gov/cdc/nbs/questionbank/repository/odse/WaRuleMetadataRepository.java
@@ -4,6 +4,9 @@ import gov.cdc.nbs.questionbank.entity.odse.WaRuleMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface WaRuleMetadataRepository extends JpaRepository<WaRuleMetadata, Long>, JpaSpecificationExecutor<WaRuleMetadata> {
 
+    List<WaRuleMetadata> findAllByWaTemplateUid(Long uid);
 }
