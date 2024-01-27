d09579d7506a124cd445c622e74745c60d27fad1
Sergei Veselev <sveselev@enquizit.com>
2024-01-26 23:59:02 -0500

    CNEDT-347 Fixes for non-general codeset values

--- libs/questionbank/src/main/java/gov/cdc/nbs/questionbank/repository/srte/ConditionCodeRepository.java
+++ libs/questionbank/src/main/java/gov/cdc/nbs/questionbank/repository/srte/ConditionCodeRepository.java
@@ -1,10 +1,8 @@
 package gov.cdc.nbs.questionbank.repository.srte;
 
 import gov.cdc.nbs.questionbank.entity.srte.ConditionCode;
-import gov.cdc.nbs.questionbank.entity.srte.ProgramAreaCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.lang.NonNull;
 
 import java.util.List;
 
